9a875d0170a4dbcdc1001d944843661b41e3b947
Medrick Zounlome <mzounlome@gmail.com>
2023-09-18 15:01:46 -0400

    testimonials

---
+++ src/app/page.tsx
@@ -0,0 +1,24 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+      
+        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-red lg:static lg:h-auto lg:w-auto lg:bg-none">
+        
+        </div>
+      </div>
+
+     
+
+      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+       
+
+      
+
+       
+      </div>
+    </main>
+  )
+}
